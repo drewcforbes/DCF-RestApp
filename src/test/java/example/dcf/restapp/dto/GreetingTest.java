6390bcb345b06fce24cacfe612ddcefeca85fefe
Drew Forbes <drewcforbes@gmail.com>
2017-08-07 21:40:27 -0400

    Tests run and start

--- src/test/java/example/dcf/restapp/dto/GreetingTest.java
+++ src/test/java/example/dcf/restapp/dto/GreetingTest.java
@@ -1,5 +1,17 @@
 package example.dcf.restapp.dto;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class GreetingTest {
+
+	@Test
+	public void testsStart() throws Exception {
+		assertTrue(true);
+	}
 
 }
