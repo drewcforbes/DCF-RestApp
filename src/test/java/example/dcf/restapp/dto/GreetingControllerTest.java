2d93b82f65771b81898851ad0cbdb8086ffb9e08
Drew Forbes <drewcforbes@gmail.com>
2017-08-07 22:12:07 -0400

    Get ready for REST Assured

--- src/test/java/example/dcf/restapp/dto/GreetingControllerTest.java
+++ src/test/java/example/dcf/restapp/dto/GreetingControllerTest.java
@@ -7,11 +7,16 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-public class GreetingTest {
+public class GreetingControllerTest {
 
 	@Test
 	public void testsStart() throws Exception {
 		assertTrue(true);
+	}
+
+	@Test
+	public void getGreetingTest() throws Exception {
+		// when(methodCall)
 	}
 
 }
